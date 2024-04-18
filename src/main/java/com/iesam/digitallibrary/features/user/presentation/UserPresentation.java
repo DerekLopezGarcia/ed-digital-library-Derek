b9b5943aab7c0bb38f85a71eb1419e6c7d07afba
DerekLopezGarcia <115727176+DerekLopezGarcia@users.noreply.github.com>
2024-04-18 18:09:09 +0200

    Dar de baja usuario en la biblioteca (#4)
    * Feat: Dar de baja un Usuario
    
    Creado caso de uso, metodos de los repositorios y opcion en el menu para dar de baja el Usuario basandonos en el Dni.

--- src/main/java/com/iesam/digitallibrary/features/user/presentation/UserPresentation.java
+++ src/main/java/com/iesam/digitallibrary/features/user/presentation/UserPresentation.java
@@ -1,13 +1,17 @@
 package com.iesam.digitallibrary.features.user.presentation;
 
 import com.iesam.digitallibrary.features.user.data.UserDataRepository;
+import com.iesam.digitallibrary.features.user.domain.DeleteUserByDniUseCase;
 import com.iesam.digitallibrary.features.user.domain.NewUserUseCase;
 import com.iesam.digitallibrary.features.user.domain.User;
-import com.iesam.digitallibrary.features.user.domain.UserRepository;
 
 public class UserPresentation {
     public static void save(User user){
         NewUserUseCase newUserUseCase = new NewUserUseCase(UserDataRepository.newInstance());
         newUserUseCase.execute(user);
+    }
+    public static void delete(String dni){
+        DeleteUserByDniUseCase deleteUserByDniUseCase = new DeleteUserByDniUseCase(UserDataRepository.newInstance());
+        deleteUserByDniUseCase.execute(dni);
     }
 }
