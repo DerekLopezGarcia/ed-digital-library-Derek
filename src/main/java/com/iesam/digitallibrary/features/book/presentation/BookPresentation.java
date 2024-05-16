3ea096bb108ea4fc59f8536f952a96af9ab5c387
DerekLopezGarcia <115727176+DerekLopezGarcia@users.noreply.github.com>
2024-05-16 17:00:50 +0200

    Feat: Libros digitales
    Menu terminado

--- src/main/java/com/iesam/digitallibrary/features/book/presentation/BookPresentation.java
+++ src/main/java/com/iesam/digitallibrary/features/book/presentation/BookPresentation.java
@@ -7,23 +7,23 @@ import com.iesam.digitallibrary.features.user.domain.ModifyUserUseCase;
 
 public class BookPresentation {
 
-    public void saveBook(Book book){
+    public static void saveBook(Book book){
         SaveBookUseCase saveBookUseCase = new SaveBookUseCase(BookDataRepository.newInstance());
         saveBookUseCase.execute(book);
     }
-    public void deleteBook(String isbn){
+    public static void deleteBook(String isbn){
         DeleteBookUseCase deleteBookUseCase = new DeleteBookUseCase(BookDataRepository.newInstance());
         deleteBookUseCase.execute(isbn);
     }
-    public Book getBook(String isbn){
+    public static Book getBook(String isbn){
         GetBookByIsbnUseCase getBookByIsbnUseCase = new GetBookByIsbnUseCase(BookDataRepository.newInstance());
         return getBookByIsbnUseCase.execute(isbn);
     }
-    public void updateBook(Book book){
+    public static void updateBook(Book book){
         ModifyBookUseCase modifyBookUseCase = new ModifyBookUseCase(BookDataRepository.newInstance());
         modifyBookUseCase.execute(book);
     }
-    public ArrayList<Book> getAllBooks(){
+    public static ArrayList<Book> getAllBooks(){
         GetAllBookUseCase getAllBookUseCase = new GetAllBookUseCase(BookDataRepository.newInstance());
         return getAllBookUseCase.execute();
     }
