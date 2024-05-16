03957e285d9f8ba65bd467ab1d6ec7a7888a106f
DerekLopezGarcia <115727176+DerekLopezGarcia@users.noreply.github.com>
2024-05-16 15:49:11 +0200

    Feat: Libros digitales
    BookDataRepository estructura terminada

--- src/main/java/com/iesam/digitallibrary/features/book/data/BookDataRepository.java
+++ src/main/java/com/iesam/digitallibrary/features/book/data/BookDataRepository.java
@@ -1,7 +1,25 @@
 package com.iesam.digitallibrary.features.book.data;
 
+import com.iesam.digitallibrary.features.book.data.local.BookFileLocalDataSource;
+import com.iesam.digitallibrary.features.book.domain.Book;
 import com.iesam.digitallibrary.features.book.domain.BookRepsoitory;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BookDataRepository implements BookRepsoitory {
+
+    public BookFileLocalDataSource bookFileLocalDataSource = new BookFileLocalDataSource();
+    public final Map<String, Book> bookMap = new HashMap<>();
+    public final ArrayList<Book> users = new ArrayList<>();
+    public static BookRepsoitory instance=null;
+    public static BookDataRepository newInstance(){
+        if(instance==null){
+            instance=new BookDataRepository();
+        }
+        return (BookDataRepository) instance;
+    }
 
 }
